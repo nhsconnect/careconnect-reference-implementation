37a7eb6aefbc80d952b936ecf4f0a89138969c64
KevinMayfield <mayfield.g.kev@gmail.com>
2017-09-06 08:22:45 +0100

    Refactoring database

---
+++ ccri-database/src/main/java/uk/nhs/careconnect/ri/entity/Terminology/ValueSetIdentifier.java
@@ -0,0 +1,34 @@
+package uk.nhs.careconnect.ri.entity.Terminology;
+
+import uk.nhs.careconnect.ri.entity.BaseIdentifier;
+
+
+@Entity
+@Table(name="ValueSetIdentifier", uniqueConstraints= @UniqueConstraint(name="PK_VALUESET_IDENTIFIER", columnNames={"VALUESET_IDENTIFIER_ID"}))
+public class ValueSetIdentifier extends BaseIdentifier {
+	
+	public ValueSetIdentifier() {
+		
+	}
+	
+	public ValueSetIdentifier(ValueSetEntity valueSetEntity) {
+		this.valueSetEntity = valueSetEntity;
+	}
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "VALUESET_IDENTIFIER_ID")
+	public Integer getIdentifierId() { return identifierId; }
+	public void setIdentifierId(Integer identifierId) { this.identifierId = identifierId; }
+	private Integer identifierId;
+	
+	
+	private ValueSetEntity valueSetEntity;
+	@ManyToOne
+	@JoinColumn (name = "VALUESET_ID",foreignKey= @ForeignKey(name="FK_VALUESET_VALUESET_IDENTIFIER"))
+	public ValueSetEntity getValueSetEntity() {
+	        return this.valueSetEntity;
+	}
+	public void setValueSetEntity(ValueSetEntity valueSetEntity) {
+	        this.valueSetEntity = valueSetEntity;
+	}
+}
