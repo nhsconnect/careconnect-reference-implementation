d32cb8a0f1a4d91e64ca62866fe49577acf89715
KevinMayfield <mayfield.g.kev@gmail.com>
2017-10-03 17:02:20 +0100

    Added 100 sample patients to base image

--- cc-core/src/main/java/uk/org/hl7/fhir/core/Stu3/CareConnectExtension.java
+++ cc-core/src/main/java/uk/org/hl7/fhir/core/Stu3/CareConnectExtension.java
@@ -2,12 +2,12 @@ package uk.org.hl7.fhir.core.Stu3;
 
 public class CareConnectExtension {
 
-    public static final String UrlEthnicCategory = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-EthnicCategory-1";
-    public static final String UrlDateRecorded = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-DateRecorded-1";
-    public static final String UrlNHSNumberVerificationStatus = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-NHSNumberVerificationStatus-1";
+    public static final String UrlEthnicCategory = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-EthnicCategory-1";
+    public static final String UrlDateRecorded = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-DateRecorded-1";
+    public static final String UrlNHSNumberVerificationStatus = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-NHSNumberVerificationStatus-1";
 
-    public static final String UrlMedicationRepeatInformation = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-MedicationRepeatInformation-1";
-    public static final String UrlMedicationStatementLastIssueDate = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-MedicationStatementLastIssueDate-1";
-    public static final String UrlMedicationSupplyType = "https://fhir.hl7.org.uk/StructureDefinition/Extension-CareConnect-MedicationSupplyType-1";
+    public static final String UrlMedicationRepeatInformation = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-MedicationRepeatInformation-1";
+    public static final String UrlMedicationStatementLastIssueDate = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-MedicationStatementLastIssueDate-1";
+    public static final String UrlMedicationSupplyType = "https://fhir.hl7.org.uk/STU3/StructureDefinition/Extension-CareConnect-MedicationSupplyType-1";
 
 }
