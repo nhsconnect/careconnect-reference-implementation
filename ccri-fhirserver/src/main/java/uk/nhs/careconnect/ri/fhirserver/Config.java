b6ee28be5546af6ff1015b7fe2e70bb96fc94380
Andy Bowes <48afe687@opayq.com>
2017-10-19 10:12:58 +0100

    Split Integration Tests

--- ccri-fhirserver/src/main/java/uk/nhs/careconnect/ri/fhirserver/Config.java
+++ ccri-fhirserver/src/main/java/uk/nhs/careconnect/ri/fhirserver/Config.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Created by kevinmayfield on 21/07/2017.
  */
@@ -20,4 +22,20 @@ public class Config {
 
     @Value("${datasource.cleardown.cron:0 19 21 * * *}")
     private String cron;
+
+    @Value("${datasource.serverBase}")
+    private String serverBase;
+
+    @PostConstruct
+    private void printConfig(){
+        System.out.println("Started with Config:" +  this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Config{" +
+                "cron='" + cron + '\'' +
+                ", serverBase='" + serverBase + '\'' +
+                '}';
+    }
 }
