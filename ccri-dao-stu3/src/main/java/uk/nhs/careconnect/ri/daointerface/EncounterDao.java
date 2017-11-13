0b8ca9f5ef0bc60a3d5e6d92ab945f23bfc75a98
KevinMayfield <mayfield.g.kev@gmail.com>
2017-11-13 16:44:15 +0000

    Search Encounter

--- ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/EncounterDao.java
+++ ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/EncounterDao.java
@@ -8,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import uk.nhs.careconnect.ri.daointerface.transforms.EncounterEntityToFHIREncounterTransformer;
 import uk.nhs.careconnect.ri.entity.encounter.EncounterEntity;
+import uk.nhs.careconnect.ri.entity.patient.PatientEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Repository
@@ -46,11 +50,51 @@ public class EncounterDao implements EncounterRepository {
 
     @Override
     public List<Encounter> search(ReferenceParam patient, DateRangeParam date, ReferenceParam episode) {
-        return null;
+        List<EncounterEntity> qryResults = searchEntity(patient, date, episode);
+        List<Encounter> results = new ArrayList<>();
+
+        for (EncounterEntity encounterEntity : qryResults)
+        {
+            // log.trace("HAPI Custom = "+doc.getId());
+            Encounter encounter = encounterEntityToFHIREncounterTransformer.transform(encounterEntity);
+            results.add(encounter);
+        }
+
+        return results;
     }
 
     @Override
     public List<EncounterEntity> searchEntity(ReferenceParam patient, DateRangeParam date, ReferenceParam episode) {
-        return null;
+        List<EncounterEntity> qryResults = null;
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+
+        CriteriaQuery<EncounterEntity> criteria = builder.createQuery(EncounterEntity.class);
+        Root<EncounterEntity> root = criteria.from(EncounterEntity.class);
+
+        List<Predicate> predList = new LinkedList<Predicate>();
+        List<Encounter> results = new ArrayList<Encounter>();
+
+        if (patient != null) {
+            Join<EncounterEntity, PatientEntity> join = root.join("patient", JoinType.LEFT);
+
+            Predicate p = builder.equal(join.get("id"),patient.getIdPart());
+            predList.add(p);
+        }
+
+
+        Predicate[] predArray = new Predicate[predList.size()];
+        predList.toArray(predArray);
+        if (predList.size()>0)
+        {
+            criteria.select(root).where(predArray);
+        }
+        else
+        {
+            criteria.select(root);
+        }
+
+        qryResults = em.createQuery(criteria).getResultList();
+        return qryResults;
     }
 }
--- ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/EpisodeOfCareDao.java
+++ ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/EpisodeOfCareDao.java
@@ -3,15 +3,21 @@ package uk.nhs.careconnect.ri.daointerface;
 import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import org.hl7.fhir.dstu3.model.EpisodeOfCare;
+
 import org.hl7.fhir.dstu3.model.IdType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import uk.nhs.careconnect.ri.daointerface.transforms.EpisodeOfCareEntityToFHIREpisodeOfCareTransformer;
+
 import uk.nhs.careconnect.ri.entity.episode.EpisodeOfCareEntity;
+import uk.nhs.careconnect.ri.entity.patient.PatientEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Repository
@@ -48,11 +54,51 @@ public class EpisodeOfCareDao implements EpisodeOfCareRepository {
 
     @Override
     public List<EpisodeOfCare> search(ReferenceParam patient, DateRangeParam date) {
-        return null;
-    }
 
+        List<EpisodeOfCareEntity> qryResults = searchEntity(patient, date);
+        List<EpisodeOfCare> results = new ArrayList<>();
+
+        for (EpisodeOfCareEntity episodeEntity : qryResults)
+        {
+            // log.trace("HAPI Custom = "+doc.getId());
+            EpisodeOfCare episode = episodeOfCareEntityToFHIREpisodeOfCareTransformer.transform(episodeEntity);
+            results.add(episode);
+        }
+
+        return results;
+    }
     @Override
     public List<EpisodeOfCareEntity> searchEntity(ReferenceParam patient, DateRangeParam date) {
-        return null;
+        List<EpisodeOfCareEntity> qryResults = null;
+
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+
+        CriteriaQuery<EpisodeOfCareEntity> criteria = builder.createQuery(EpisodeOfCareEntity.class);
+        Root<EpisodeOfCareEntity> root = criteria.from(EpisodeOfCareEntity.class);
+
+        List<Predicate> predList = new LinkedList<Predicate>();
+        List<EpisodeOfCare> results = new ArrayList<EpisodeOfCare>();
+
+        if (patient != null) {
+            Join<EpisodeOfCareEntity, PatientEntity> join = root.join("patient", JoinType.LEFT);
+
+            Predicate p = builder.equal(join.get("id"),patient.getIdPart());
+            predList.add(p);
+        }
+
+
+        Predicate[] predArray = new Predicate[predList.size()];
+        predList.toArray(predArray);
+        if (predList.size()>0)
+        {
+            criteria.select(root).where(predArray);
+        }
+        else
+        {
+            criteria.select(root);
+        }
+
+        qryResults = em.createQuery(criteria).getResultList();
+        return qryResults;
     }
 }
