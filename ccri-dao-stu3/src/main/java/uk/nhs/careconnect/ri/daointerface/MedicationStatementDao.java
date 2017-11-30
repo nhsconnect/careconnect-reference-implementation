a41efb7ba6997943e01331b9d33a0d55c573bcf8
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2017-11-30 15:27:17 +0000

    MedicationStatement fix

--- ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/MedicationStatementDao.java
+++ ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/MedicationStatementDao.java
@@ -5,9 +5,13 @@ import ca.uhn.fhir.rest.param.DateRangeParam;
 import ca.uhn.fhir.rest.param.ReferenceParam;
 import ca.uhn.fhir.rest.param.TokenParam;
 import org.hl7.fhir.dstu3.model.IdType;
+import org.hl7.fhir.dstu3.model.MedicationRequest;
 import org.hl7.fhir.dstu3.model.MedicationStatement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import uk.nhs.careconnect.ri.daointerface.transforms.MedicationRequestEntityToFHIRMedicationStatementTransformer;
 import uk.nhs.careconnect.ri.entity.location.LocationEntity;
+import uk.nhs.careconnect.ri.entity.medication.MedicationRequestEntity;
 import uk.nhs.careconnect.ri.entity.medication.MedicationStatementEntity;
 
 import javax.persistence.EntityManager;
@@ -15,6 +19,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -23,6 +28,13 @@ public class MedicationStatementDao implements MedicationStatementRepository {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MedicationRequestRepository prescriptionDao;
+
+    @Autowired
+    private MedicationRequestEntityToFHIRMedicationStatementTransformer
+            medicationRequestEntityToFHIRMedicationStatementTransformer;
 
     @Override
     public void save(FhirContext ctx, MedicationStatementEntity statement) {
@@ -34,7 +46,7 @@ public class MedicationStatementDao implements MedicationStatementRepository {
 
         CriteriaBuilder qb = em.getCriteriaBuilder();
         CriteriaQuery<Long> cq = qb.createQuery(Long.class);
-        cq.select(qb.count(cq.from(MedicationStatementEntity.class)));
+        cq.select(qb.count(cq.from(MedicationRequestEntity.class)));
         //cq.where(/*your stuff*/);
         return em.createQuery(cq).getSingleResult();
     }
@@ -52,12 +64,23 @@ public class MedicationStatementDao implements MedicationStatementRepository {
 
     @Override
     public MedicationStatement create(FhirContext ctx,MedicationStatement statement, IdType theId, String theConditional) {
+
         return null;
     }
 
     @Override
     public List<MedicationStatement> search(FhirContext ctx,ReferenceParam patient, DateRangeParam effectiveDate, TokenParam status) {
-        return null;
+        List<MedicationRequestEntity> prescriptions = prescriptionDao.searchEntity(ctx,patient,null,effectiveDate,status,null);
+        List<MedicationStatement> results = new ArrayList<>();
+
+        for (MedicationRequestEntity prescriptionEntity : prescriptions)
+        {
+            // log.trace("HAPI Custom = "+doc.getId());
+            MedicationStatement medicationStatement =  medicationRequestEntityToFHIRMedicationStatementTransformer.transform(prescriptionEntity);
+            results.add(medicationStatement);
+        }
+        return results;
+
     }
 
     @Override
---
+++ ccri-dao-stu3/src/main/java/uk/nhs/careconnect/ri/daointerface/transforms/MedicationRequestEntityToFHIRMedicationStatementTransformer.java
@@ -0,0 +1,179 @@
+package uk.nhs.careconnect.ri.daointerface.transforms;
+
+
+import org.apache.commons.collections4.Transformer;
+import org.hl7.fhir.dstu3.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.nhs.careconnect.ri.entity.BaseAddress;
+import uk.nhs.careconnect.ri.entity.medication.MedicationRequestDosage;
+import uk.nhs.careconnect.ri.entity.medication.MedicationRequestEntity;
+import uk.nhs.careconnect.ri.entity.medication.MedicationRequestIdentifier;
+import uk.org.hl7.fhir.core.Stu3.CareConnectExtension;
+import uk.org.hl7.fhir.core.Stu3.CareConnectProfile;
+import uk.org.hl7.fhir.core.Stu3.CareConnectSystem;
+
+
+@Component
+public class MedicationRequestEntityToFHIRMedicationStatementTransformer implements Transformer<MedicationRequestEntity, MedicationStatement> {
+
+    private final Transformer<BaseAddress, Address> addressTransformer;
+
+    public MedicationRequestEntityToFHIRMedicationStatementTransformer(@Autowired Transformer<BaseAddress, Address> addressTransformer) {
+        this.addressTransformer = addressTransformer;
+    }
+
+    @Override
+    public MedicationStatement transform(final MedicationRequestEntity medicationRequestEntity) {
+        final MedicationStatement medicationStatement = new MedicationStatement();
+
+        Meta meta = new Meta().addProfile(CareConnectProfile.MedicationStatement_1);
+
+        if (medicationRequestEntity.getUpdated() != null) {
+            meta.setLastUpdated(medicationRequestEntity.getUpdated());
+        }
+        else {
+            if (medicationRequestEntity.getCreated() != null) {
+                meta.setLastUpdated(medicationRequestEntity.getCreated());
+            }
+        }
+        medicationStatement.setMeta(meta);
+
+        for(MedicationRequestIdentifier identifier : medicationRequestEntity.getIdentifiers())
+        {
+            medicationStatement.addIdentifier()
+                    .setSystem(identifier.getSystem().getUri())
+                    .setValue(identifier.getValue());
+        }
+        if (medicationRequestEntity.getPatient() != null) {
+            medicationStatement.setSubject(new Reference("Patient/"+medicationRequestEntity.getPatient().getId()));
+        }
+
+        if (medicationRequestEntity.getCategoryCode() != null) {
+            medicationStatement.getCategory().addCoding()
+                    .setDisplay(medicationRequestEntity.getCategoryCode().getDisplay())
+                    .setSystem(medicationRequestEntity.getCategoryCode().getSystem())
+                    .setCode(medicationRequestEntity.getCategoryCode().getCode());
+        }
+        if (medicationRequestEntity.getContextEpisodeOfCare() != null) {
+            medicationStatement.setContext(new Reference("EpisodeOfCare/"+medicationRequestEntity.getContextEpisodeOfCare().getId()));
+        }
+        if (medicationRequestEntity.getContextEncounter() != null) {
+            medicationStatement.setContext(new Reference("Encounter/"+medicationRequestEntity.getContextEncounter().getId()));
+        }
+
+        medicationStatement.addDerivedFrom().setReference("MedicationOrder/"+medicationRequestEntity.getId());
+
+        medicationStatement.setTaken(MedicationStatement.MedicationStatementTaken.Y);
+
+
+        if (medicationRequestEntity.getMedicationCode() != null) {
+
+                CodeableConcept medication = new CodeableConcept();
+                medication.addCoding()
+                    .setDisplay(medicationRequestEntity.getMedicationCode().getDisplay())
+                    .setSystem(medicationRequestEntity.getMedicationCode().getSystem())
+                    .setCode(medicationRequestEntity.getMedicationCode().getCode());
+                medicationStatement.setMedication(medication);
+
+        }
+        if (medicationRequestEntity.getReasonCode() != null) {
+            medicationStatement.addReasonCode().addCoding()
+                    .setDisplay(medicationRequestEntity.getReasonCode().getDisplay())
+                    .setSystem(medicationRequestEntity.getReasonCode().getSystem())
+                    .setCode(medicationRequestEntity.getReasonCode().getCode());
+        }
+        if (medicationRequestEntity.getReasonCondition() != null) {
+            medicationStatement.addReasonReference(new Reference(("Condition/"+medicationRequestEntity.getReasonCondition().getId())));
+        }
+
+        if (medicationRequestEntity.getReasonObservation() != null) {
+            medicationStatement.addReasonReference(new Reference(("Observation/"+medicationRequestEntity.getReasonObservation().getId())));
+        }
+
+        for (MedicationRequestDosage dosageEntity : medicationRequestEntity.getDosages()) {
+            Dosage dosage = medicationStatement.addDosage();
+            if (dosageEntity.getAsNeededBoolean() != null) {
+                dosage.setAsNeeded(new BooleanType().setValue(dosageEntity.getAsNeededBoolean()));
+            }
+            if (dosageEntity.getAdditionalInstructionCode() != null) {
+                dosage.addAdditionalInstruction().addCoding()
+                        .setDisplay(dosageEntity.getAdditionalInstructionCode().getDisplay())
+                        .setSystem(dosageEntity.getAdditionalInstructionCode().getSystem())
+                        .setCode(dosageEntity.getAdditionalInstructionCode().getCode());
+            }
+            if (dosageEntity.getAsNeededCode()!=null) {
+                CodeableConcept concept = new CodeableConcept();
+                concept.addCoding() .setDisplay(dosageEntity.getAsNeededCode().getDisplay())
+                        .setSystem(dosageEntity.getAsNeededCode().getSystem())
+                        .setCode(dosageEntity.getAsNeededCode().getCode());
+                dosage.setAsNeeded(concept);
+            }
+            if (dosageEntity.getMethodCode()!=null) {
+                dosage.getMethod().addCoding()
+                        .setDisplay(dosageEntity.getMethodCode().getDisplay())
+                        .setSystem(dosageEntity.getMethodCode().getSystem())
+                        .setCode(dosageEntity.getMethodCode().getCode());
+            }
+            if (dosageEntity.getSiteCode()!=null) {
+                dosage.getSite().addCoding()
+                        .setDisplay(dosageEntity.getSiteCode().getDisplay())
+                        .setSystem(dosageEntity.getSiteCode().getSystem())
+                        .setCode(dosageEntity.getSiteCode().getCode());
+            }
+            if (dosageEntity.getRouteCode()!=null) {
+                dosage.getRoute().addCoding()
+                        .setDisplay(dosageEntity.getRouteCode().getDisplay())
+                        .setSystem(dosageEntity.getRouteCode().getSystem())
+                        .setCode(dosageEntity.getRouteCode().getCode());
+            }
+            if (dosageEntity.getPatientInstruction() != null) {
+                dosage.setPatientInstruction(dosageEntity.getPatientInstruction());
+            }
+            if (dosageEntity.getOtherText() != null) {
+                dosage.setText(dosageEntity.getOtherText());
+            }
+            if (dosageEntity.getDoseRangeLow() != null || dosageEntity.getDoseRangeHigh() != null) {
+                Range range = new Range();
+                if (dosageEntity.getDoseRangeLow() != null) {
+                    SimpleQuantity qty = new SimpleQuantity();
+                    qty.setValue(dosageEntity.getDoseRangeLow());
+                    if (dosageEntity.getDoseLowUnitOfMeasure() != null) {
+                        qty.setCode(dosageEntity.getDoseLowUnitOfMeasure().getCode());
+                        qty.setSystem(dosageEntity.getDoseLowUnitOfMeasure().getSystem());
+                        qty.setUnit(dosageEntity.getDoseLowUnitOfMeasure().getDisplay());
+                    }
+                    range.setLow(qty);
+                }
+                if (dosageEntity.getDoseRangeHigh() != null) {
+                    SimpleQuantity qty = new SimpleQuantity();
+                    qty.setValue(dosageEntity.getDoseRangeHigh());
+                    if (dosageEntity.getDoseHighUnitOfMeasure() != null) {
+                        qty.setCode(dosageEntity.getDoseHighUnitOfMeasure().getCode());
+                        qty.setSystem(dosageEntity.getDoseHighUnitOfMeasure().getSystem());
+                        qty.setUnit(dosageEntity.getDoseHighUnitOfMeasure().getDisplay());
+                    }
+                    range.setHigh(qty);
+                }
+                dosage.setDose(range);
+            } else {
+                if (dosageEntity.getDoseQuantity() != null) {
+                    SimpleQuantity qty = new SimpleQuantity();
+                    qty.setValue(dosageEntity.getDoseQuantity());
+                    if (dosageEntity.getDoseUnitOfMeasure() != null) {
+                        qty.setCode(dosageEntity.getDoseUnitOfMeasure().getCode());
+                        qty.setSystem(dosageEntity.getDoseUnitOfMeasure().getSystem());
+                        qty.setUnit(dosageEntity.getDoseUnitOfMeasure().getDisplay());
+                    }
+                    dosage.setDose(qty);
+                }
+            }
+
+        }
+
+        medicationStatement.setId(medicationRequestEntity.getId().toString());
+
+        return medicationStatement;
+
+    }
+}
