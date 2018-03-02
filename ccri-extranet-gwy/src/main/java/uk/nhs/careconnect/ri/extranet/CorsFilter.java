fd93da5f92e03893e1116fd434b0a271ec65ac98
KevinMayfield <mayfield.g.kev@gmail.com>
2018-03-02 16:28:08 +0000

    added cors filter to extranet

---
+++ ccri-extranet-gwy/src/main/java/uk/nhs/careconnect/ri/extranet/CorsFilter.java
@@ -0,0 +1,37 @@
+package uk.nhs.careconnect.ri.extranet;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class CorsFilter implements Filter {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CorsFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+       // log.info(req.getContentType());
+       // log.info(req.getRemoteAddr());
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "X-FHIR-Starter,authorization,Prefer,Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers");
+        if (!"OPTIONS".equals(request.getMethod())) {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
