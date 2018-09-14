f1068eb35d9b77d3d5f94ac6774688fe72e0f8db
KevinMayfield <mayfield.g.kev@gmail.com>
2018-09-14 13:42:40 +0100

    CCRI UI move to springboot

---
+++ ccri-management/src/main/java/uk/nhs/careconnect/ri/management/ManagementApplication.java
@@ -0,0 +1,12 @@
+package uk.nhs.careconnect.ri.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+}
