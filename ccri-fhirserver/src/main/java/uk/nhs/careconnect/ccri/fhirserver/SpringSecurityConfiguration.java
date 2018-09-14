07b9f3cfe4299a6f4464892acd2acb245b0d8ba3
KevinMayfield <mayfield.g.kev@gmail.com>
2018-09-14 11:44:38 +0100

    Spring security for post and put

--- ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/SpringSecurityConfiguration.java
+++ ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/SpringSecurityConfiguration.java
@@ -3,6 +3,7 @@ package uk.nhs.careconnect.ccri.fhirserver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,6 +32,8 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/").permitAll()
+                .antMatchers(HttpMethod.PUT, "/").permitAll()
                 .antMatchers("/error").permitAll()
                 .antMatchers("/jolokia/**").hasRole("ACTUATOR")
                 .and().httpBasic();
