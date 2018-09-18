897017aedb407cbb270e9f7f96822c8d72a58992
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2018-09-18 11:26:10 +0100

    Replacement of hapi ui overlay

--- ccri-management/src/main/java/uk/nhs/careconnect/ri/management/ManagementApplication.java
+++ ccri-management/src/main/java/uk/nhs/careconnect/ri/management/ManagementApplication.java
@@ -19,10 +19,9 @@ public class ManagementApplication {
         System.setProperty("hawtio.role","MANAGER");
         System.setProperty("management.security.enabled","false");
         System.setProperty("server.port", "8187");
-        System.setProperty("server.context-path", "/ccri");
+       // System.setProperty("server.context-path", "/ccri");
         //System.setProperty("server.servlet.context-path", "/ccri");
         System.setProperty("management.contextPath","");
-        System.setProperty("spring.mvc.static-path-pattern","/resources/**");
 
         SpringApplication.run(ManagementApplication.class, args);
     }
