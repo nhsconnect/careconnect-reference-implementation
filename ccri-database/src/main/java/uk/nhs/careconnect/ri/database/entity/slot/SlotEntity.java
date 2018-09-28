3aba7a92ac08c6c09fc3c52bb7fb1b8f5d2f10b9
KevinMayfield <mayfield.g.kev@gmail.com>
2018-09-28 14:48:43 +0100

    merged a2i with master

--- ccri-database/src/main/java/uk/nhs/careconnect/ri/database/entity/slot/SlotEntity.java
+++ ccri-database/src/main/java/uk/nhs/careconnect/ri/database/entity/slot/SlotEntity.java
@@ -27,15 +27,15 @@ public class SlotEntity extends BaseResource {
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="SERVICE_CATEGORY",nullable = false)
+    @JoinColumn(name="SERVICE_CATEGORY_ID",nullable = false)
     private ConceptEntity serviceCategory;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="APPOINTMENT_TYPE",nullable = true)
+    @JoinColumn(name="APPOINTMENT_TYPE_ID",nullable = true)
     private ConceptEntity appointmentType;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="SCHEDULE",nullable = false)
+    @JoinColumn(name="SCHEDULE_ID",nullable = false)
     private ScheduleEntity schedule;
 
     @Temporal(TemporalType.TIMESTAMP)
