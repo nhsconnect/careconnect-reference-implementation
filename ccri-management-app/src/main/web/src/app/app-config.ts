fa5bcbc8c8155d08a9bfda94a1ad0b57c3ee961a
kevinmayfieldAire <KevinMayfield@users.noreply.github.com>
2018-10-30 17:26:51 +0000

    Added SMART on FHIR and OAuth2 security

--- ccri-management-app/src/main/web/src/app/app-config.ts
+++ ccri-management-app/src/main/web/src/app/app-config.ts
@@ -2,24 +2,42 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {FhirService} from "./service/fhir.service";
 import {AuthService} from "./service/auth.service";
+import {environment} from "../environments/environment";
+import {root} from "rxjs/internal-compatibility";
 
 @Injectable()
 export class AppConfig {
 
-    constructor(private http: HttpClient, private fhirServer : FhirService, private authService : AuthService) {}
+    constructor(private http: HttpClient, private fhirService : FhirService) {}
 
     load() {
         // console.log('hello App' + document.baseURI);
         // only run if not localhost
         if (!document.baseURI.includes('localhost')) {
             this.http.get<any>(document.baseURI + 'camel/config/http').subscribe(result => {
-                    console.log(result);
-                    this.fhirServer.setRootUrl(result.fhirServer);
-                    if (this.authService.isLoggedOn()) {
-                       this.authService.setBaseUrlOAuth2();
+                  console.log(result);
+
+                  const access_token = localStorage.getItem('access_token_' + environment.oauth2.client_id);
+                  let rootUrl : string = result.fhirServer;
+                  if (access_token === "" || access_token === null) {
+                    //
+                  } else {
+                    if (result.fhirServer.includes('8183/ccri-fhir')) {
+                      let newbaseUrl : string = 'https://data.developer-test.nhs.uk/ccri-smartonfhir/STU3';
+                      console.log('swapping to smartonfhir instance: '+newbaseUrl);
+                      rootUrl = newbaseUrl;
                     }
-                    this.fhirServer.setGPCNRLSUrl(document.baseURI);
-                },
+                    else {
+                      if (result.fhirServer.includes('ccri-fhir')) {
+                        let newbaseUrl : string = this.fhirService.getBaseUrl().replace('ccri-fhir','ccri-smartonfhir');
+                        console.log('swapping to smartonfhir instance: '+ newbaseUrl);
+                        rootUrl = newbaseUrl;
+                      }
+                    }
+                  }
+                  this.fhirService.setRootUrl(rootUrl);
+                  this.fhirService.setGPCNRLSUrl(document.baseURI);
+              },
                 () => {
                     console.log('No server deteted');
                     // this.fhirServer.setRootUrl('http://127.0.0.1:8183/ccri-fhir/STU3');
