2238290bd4cceba9e5764b31a12b37c9563496d9
KevinMayfield <mayfield.g.kev@gmail.com>
2019-02-09 11:52:55 +0000

    Updated to hapi 3.7, hawtio, spring-boot 2 and hibernate to match hapi

--- ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/provider/ValueSetProvider.java
+++ ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/provider/ValueSetProvider.java
@@ -12,40 +12,29 @@ import ca.uhn.fhir.rest.server.IResourceProvider;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
 import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.ExchangePattern;
-import org.apache.camel.Processor;
-import org.apache.camel.ProducerTemplate;
+
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
+
 import org.apache.http.impl.client.HttpClientBuilder;
-/*import org.apache.camel.Exchange;
-import org.apache.camel.ExchangePattern;
-import org.apache.camel.Processor;
-import org.apache.camel.ProducerTemplate;*/
+
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
-import org.hl7.fhir.dstu3.model.Parameters;
-import org.hl7.fhir.dstu3.model.Patient;
+
 import org.hl7.fhir.dstu3.model.ValueSet;
-import org.hl7.fhir.dstu3.model.ValueSet.ValueSetComposeComponent;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.nhs.careconnect.ccri.fhirserver.OperationOutcomeFactory;
-import uk.nhs.careconnect.ccri.fhirserver.ProviderResponseLibrary;
+
 import uk.nhs.careconnect.ri.database.daointerface.ValueSetRepository;
-//import uk.nhs.careconnect.ri.messaging.support.ProviderResponseLibrary;
 
 import javax.servlet.http.HttpServletRequest;
 
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.UnknownHostException;
@@ -54,8 +43,6 @@ import java.util.List;
 @Component
 public class ValueSetProvider implements IResourceProvider {
 
-	@Autowired
-	CamelContext context;
 
 	@Autowired
 	FhirContext ctx;
