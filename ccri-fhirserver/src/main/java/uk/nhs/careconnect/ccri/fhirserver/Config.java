191ba07385c6e3f5969a77500ad058ae0174b4fc
KevinMayfield <mayfield.g.kev@gmail.com>
2019-03-06 09:59:39 +0000

    Reenable of Bundle

--- ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/Config.java
+++ ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/Config.java
@@ -1,11 +1,20 @@
 package uk.nhs.careconnect.ccri.fhirserver;
 
+
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.validation.FhirValidator;
+
+import org.hl7.fhir.dstu3.hapi.validation.FhirInstanceValidator;
+import org.hl7.fhir.dstu3.hapi.validation.ValidationSupportChain;
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.nhs.careconnect.ccri.fhirserver.provider.DatabaseBackedPagingProvider;
+import uk.org.hl7.fhir.validation.stu3.CareConnectProfileValidationSupport;
+import uk.org.hl7.fhir.validation.stu3.SNOMEDUKMockValidationSupport;
+import org.hl7.fhir.dstu3.hapi.validation.DefaultProfileValidationSupport;
 
 
 /**
@@ -24,8 +33,41 @@ public class Config {
         			return retVal;
         }
 
+    @Autowired
+    FhirContext ctx;
+
     @Value("${ccri.server.base}")
     private String serverBase;
+
+
+    @Bean(name="fhirValidator")
+    public FhirValidator fhirValidator () {
+
+        FhirValidator val = ctx.newValidator();
+
+        val.setValidateAgainstStandardSchema(true);
+
+        // todo reactivate once this is fixed https://github.com/nhsconnect/careconnect-reference-implementation/issues/36
+        val.setValidateAgainstStandardSchematron(false);
+
+        DefaultProfileValidationSupport defaultProfileValidationSupport = new DefaultProfileValidationSupport();
+
+        FhirInstanceValidator instanceValidator = new FhirInstanceValidator(defaultProfileValidationSupport);
+        val.registerValidatorModule(instanceValidator);
+
+
+        ValidationSupportChain validationSupportChain = new ValidationSupportChain();
+
+        validationSupportChain.addValidationSupport(new DefaultProfileValidationSupport());
+        validationSupportChain.addValidationSupport(new CareConnectProfileValidationSupport(ctx));
+        validationSupportChain.addValidationSupport(new SNOMEDUKMockValidationSupport());
+
+        instanceValidator.setValidationSupport(validationSupportChain);
+
+
+
+        return val;
+    }
 
 
 }
--- ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/provider/ResourceTestProvider.java
+++ ccri-fhirserver/src/main/java/uk/nhs/careconnect/ccri/fhirserver/provider/ResourceTestProvider.java
@@ -7,38 +7,52 @@ import ca.uhn.fhir.rest.annotation.Validate;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.api.ValidationModeEnum;
 import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
-import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
+import ca.uhn.fhir.validation.FhirValidator;
+import ca.uhn.fhir.validation.ValidationResult;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.OperationOutcome;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import uk.nhs.careconnect.ccri.fhirserver.OperationOutcomeFactory;
 import uk.nhs.careconnect.ccri.fhirserver.ProviderResponseLibrary;
-import uk.nhs.careconnect.fhir.OperationOutcomeException;
+
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+
+import static org.fusesource.jansi.Ansi.ansi;
 
 @Component
 public class ResourceTestProvider {
 
 	@Value("${ccri.validate_flag}")
 	private Boolean validate_flag;
-	
+
+	@Value("${ccri.validate_use_tkw}")
+	private Boolean tkw_flag;
+
+
 	@Value("${ccri.tkw_server}")
 	private String tkw_server;
 	
     @Autowired
     FhirContext ctx;
+
+ //   @Autowired
+//	FhirInstanceValidator instanceValidator;
+
+    @Autowired
+    FhirValidator val;
     
 	HttpResponse response;
 	Reader reader;
@@ -67,41 +81,102 @@ public class ResourceTestProvider {
 			return retVal;
 		}
 
-		final HttpClient client1 = getHttpClient();
-        final HttpPost request = new HttpPost(tkw_server);
-        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/fhir+json");
-        request.setHeader(HttpHeaders.ACCEPT, "application/fhir+json");
-        try {
+
+
+		if (tkw_flag) {
+
+			final HttpClient client1 = getHttpClient();
+			final HttpPost request = new HttpPost(tkw_server);
+			request.setHeader(HttpHeaders.CONTENT_TYPE, "application/fhir+json");
+			request.setHeader(HttpHeaders.ACCEPT, "application/fhir+json");
+			try {
 
 				request.setEntity(new StringEntity(ctx.newJsonParser().encodeResourceToString(resourceToValidate)));
 
 				response = client1.execute(request);
 				reader = new InputStreamReader(response.getEntity().getContent());
-				
-				 IBaseResource resource = ctx.newJsonParser().parseResource(reader);
-				 if(resource instanceof OperationOutcome)
-		         {
-		            OperationOutcome operationOutcome = (OperationOutcome) resource;
-		            log.info("Issue Count = " + operationOutcome.getIssue().size());
-		            log.info(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(operationOutcome));
-		            retVal.setOperationOutcome(operationOutcome);
-		         }
-		         else
-		         {
-		            throw new InternalErrorException("Server Error", (OperationOutcome) resource);
-		         }
+
+				IBaseResource resource = ctx.newJsonParser().parseResource(reader);
+				if (resource instanceof OperationOutcome) {
+					OperationOutcome operationOutcome = (OperationOutcome) resource;
+					log.info("Issue Count = " + operationOutcome.getIssue().size());
+					log.info(ctx.newJsonParser().setPrettyPrint(true).encodeResourceToString(operationOutcome));
+					retVal.setOperationOutcome(operationOutcome);
+				} else {
+					throw new InternalErrorException("Server Error", (OperationOutcome) resource);
+				}
 
 
-        	} catch (Exception e) {
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 
 				e.printStackTrace();
 				// https://airelogic-apilabs.atlassian.net/browse/ALP4-922
-				ProviderResponseLibrary.handleException(retVal,e);
+				ProviderResponseLibrary.handleException(retVal, e);
 			}
+		} else {
+			retVal.setOperationOutcome(validateResource(resourceToValidate));
+			if (resourceToValidate instanceof Bundle) {
 
+			} else {
+
+			}
+		}
         return retVal;
 
 }
+
+
+	public OperationOutcome validateResource(IBaseResource resource) {
+		OperationOutcome outcome = new OperationOutcome();
+		ValidationResult results = val.validateWithResult(resource);
+
+		outcome = (OperationOutcome) results.toOperationOutcome();
+		/*
+		if (bundle != null) {
+			for (Bundle.BundleEntryComponent entry : bundle.getEntry()) {
+				Resource resource = entry.getResource();
+
+				ValidationResult results = val.validateWithResult(resource);
+
+
+				outcome = (OperationOutcome) results.toOperationOutcome();
+
+
+				StringBuilder b = new StringBuilder("Validation results:" + ansi().boldOff());
+				int count = 0;
+				for (SingleValidationMessage next : results.getMessages()) {
+					// We are not used a UK Terminology server and can't expand resources at present. Hence ignoring these errors/warnings
+					if (next.getMessage().contains("and a code from this value set is required") && next.getMessage().contains(CareConnectSystem.SNOMEDCT)) {
+						System.out.println("match **");
+					} else if (next.getMessage().contains("a code is required from this value set") && next.getMessage().contains(CareConnectSystem.SNOMEDCT)) {
+						System.out.println("match ** ** ");
+					} else if (next.getMessage().contains("and a code is recommended to come from this value set") && next.getMessage().contains(CareConnectSystem.SNOMEDCT)) {
+						System.out.println("match ** ** **");
+					} else if (next.getMessage().contains("and a code is recommended to come from this value set") && next.getMessage().contains("https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-ConditionCategory-1")) {
+						System.out.println("match ** ** **");
+					} else if (next.getMessage().contains("and a code should come from this value set unless it has no suitable code") && next.getMessage().contains(CareConnectSystem.SNOMEDCT)) {
+						System.out.println("match ** ** ** **");
+					} else if (next.getMessage().contains("and a code should come from this value set unless it has no suitable code") && next.getMessage().contains("https://fhir.hl7.org.uk/STU3/ValueSet/CareConnect-ConditionCategory-1")) {
+						System.out.println("match ** ** ** **");
+					} else if (next.getMessage().contains("path Patient.name (fhirPath = true and (use memberOf")) {
+						System.out.println("** ** ** Code Issue ValueSet expansion not implemented in instanceValidator" );
+					} else if (next.getMessage().contains("Error Multiple filters not handled yet")) {
+						System.out.println("** ** ** multiple filters in ValueSet not implemented" );
+					} else {
+
+						count++;
+
+						String message ="HL7 FHIR CareConnect Validation - "+next.getSeverity() + " " + next.getMessage()+ " Resource="+resource.getResourceType().toString()+"/"+resource.getIdElement().getIdPart();
+						System.out.println(message);
+						outcome.addIssue()
+								.setCode(next.getSeverity())
+					}
+				}
+
+			}
+		}*/
+		return outcome;
+	}
 
 }
